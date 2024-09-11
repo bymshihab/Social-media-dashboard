594f4e5b43b24cbb8baacab18f28d8c6af544e78
Md. Khaled Jubair <khaled.jubair@ndessl.local>
2024-09-11 19:14:34 +0600

    done

--- src/app/features/posts/post-create/post-create.component.ts
+++ src/app/features/posts/post-create/post-create.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
-  styleUrl: './post-create.component.css'
+  styleUrl: './post-create.component.css',
 })
 export class PostCreateComponent {
+  @ViewChild('myModal') myModal!: ElementRef<HTMLDialogElement>;
 
+  openModal() {
+    const modal = this.myModal.nativeElement;
+    if (modal instanceof HTMLDialogElement) {
+      modal.showModal();
+    }
+  }
+
+  closeModal() {
+    const modal = this.myModal.nativeElement;
+    if (modal instanceof HTMLDialogElement) {
+      modal.close();
+    }
+  }
 }
